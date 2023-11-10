52a757c6910fc11ab0a8d9025cc50ecbad3c6d06
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-11-09 22:47:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+
+let logado = document.querySelector("#logado");
+logado.innerHTML = 'Olá ${userLogado.nome}';
+
+if(localStorage.getItem("token") == null){
+  alert("Você precisa estar logado para acessar essa página");
+  window.location.href = "https://cdpn.io/thicode/debug/ZELzYxV/nqkwzJowRxA";
+}
+
+function sair(){
+  localStorage.removeItem("token");
+  localStorage.removeItem("userLogado");
+  window.location.href = "https://cdpn.io/thicode/debug/ZELzYxV/nqkwzJowRxA";
+}
+
 
